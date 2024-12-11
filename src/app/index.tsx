e7c5e50f7cf824add33cb5b712d0d6393caf6e68
jeronimofsneto <jeronimo@ufdpar.edu.br>
2024-12-11 20:49:07 -0300

    feat: add tabler e svg

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Steps } from "@/components/steps";
+import { Welcome } from "@/components/welcome";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 30, color: "blue" }}>Ola Mundo</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
     </View>
   );
 }
